b906f1d414521b9f4031e72bd114759c6ff580c6
Raphi <raphael.antonietti@liip.ch>
2019-05-05 21:22:51 +0200

    etter mobile usability

--- utilitiesformtg.js
+++ utilitiesformtg.js
@@ -1,7 +1,21 @@
 function swapimage(id, front, back) {
     if (document.getElementById(id).src == front) {
-        document.getElementById(id).src=back;        
+        document.getElementById(id).src=back;
     } else {
         document.getElementById(id).src=front;
     }
 };
+
+//bind touch event
+//TODO: check how we do this with dynamic content
+document.querySelectorAll(".scryfall_hover_img").forEach(function(scryFallWrapper){
+	scryFallWrapper.addEventListener("touchstart", function (event) {
+    event.preventDefault();
+    //remove all other classes so we can actively close stuff
+    document.querySelectorAll(".scryfall_hover_img").forEach(function(scryFallWrapper){
+      scryFallWrapper.classList.remove("mobileDisplayScryfallImage")
+    })
+    //show this
+    event.currentTarget.classList.add("mobileDisplayScryfallImage");
+  });
+})
