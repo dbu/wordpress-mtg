1d432c6c3bec657a1addb0d80f953a2918b342b9
Raphi <raphael.antonietti@liip.ch>
2019-05-05 21:29:32 +0200

    nodelist polyfill

--- utilitiesformtg.js
+++ utilitiesformtg.js
@@ -1,3 +1,8 @@
+//nodelist foreach polyfill
+if (window.NodeList && !NodeList.prototype.forEach) {
+  NodeList.prototype.forEach = Array.prototype.forEach;
+}
+
 function swapimage(id, front, back) {
     if (document.getElementById(id).src == front) {
         document.getElementById(id).src=back;
