7a6add23d23bd99cb998fd1a28dc69cdf9745791
Raphi <raphael.antonietti@liip.ch>
2019-05-19 15:18:24 +0200

    Fix Styling bug and force bindings

--- utilitiesformtg.js
+++ utilitiesformtg.js
@@ -13,14 +13,17 @@ function swapimage(id, front, back) {
 
 //bind touch event
 //TODO: check how we do this with dynamic content
-document.querySelectorAll(".scryfall_hover_img").forEach(function(scryFallWrapper){
-	scryFallWrapper.addEventListener("touchstart", function (event) {
-    event.preventDefault();
-    //remove all other classes so we can actively close stuff
-    document.querySelectorAll(".scryfall_hover_img").forEach(function(scryFallWrapper){
-      scryFallWrapper.classList.remove("mobileDisplayScryfallImage")
-    })
-    //show this
-    event.currentTarget.classList.add("mobileDisplayScryfallImage");
-  });
-})
+setInterval(function  () {
+  document.querySelectorAll(".scryfall_hover_img").forEach(function(scryFallWrapper){
+    scryFallWrapper.addEventListener("touchstart", function (event) {
+      event.preventDefault();
+      //remove all other classes so we can actively close stuff
+      document.querySelectorAll(".scryfall_hover_img").forEach(function(scryFallWrapper){
+        scryFallWrapper.classList.remove("mobileDisplayScryfallImage")
+      })
+      //show this
+      event.currentTarget.classList.add("mobileDisplayScryfallImage");
+    });
+  })
+}, 500);
+
